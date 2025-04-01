f6ebabb658c7c7bbaf15e4ba99f2b7202591e7c9
Erling Jellum <erling.jellum@gmail.com>
2025-04-01 08:29:50 +0200

    Add Blinky example

---
+++ Blinky/build.ps1
@@ -0,0 +1,21 @@
+# This PowerShell3 script will run the Lingua Franca Compiler (LFC)
+# on the file specified in $MAIN. It is meant to be invoked pre-build
+# by Keil uVision 5.
+
+$MAIN="Blinky"
+
+try {
+    $LF_MAIN=Join-Path -Path $PSScriptRoot -ChildPath "src\$MAIN.lf"
+    $LFC = Join-Path -Path $Env:REACTOR_UC_PATH -ChildPath "lfc\bin\lfc-dev.ps1"
+
+    if (-not $Env:REACTOR_UC_PATH) {
+        throw "Environment variable REACTOR_UC_PATH not set"
+    }
+    Write-Host "Running command $LFC $LF_MAIN"
+    & $LFC $LF_MAIN
+}
+catch {
+    Write-Error "Error occurred during code-generation: $_"
+    exit
+}
+
